a84485d844b56b39802cea73346c0c4db1f78639
fcknbstrd <eric.berger.82@googlemail.com>
2026-09-21 16:32:14 +0200

    Bugfix: increased address space for 32-bit application

--- BilliardMaster.dpr
+++ BilliardMaster.dpr
@@ -3,8 +3,12 @@ program BilliardMaster;
 uses
   System.StartUpCopy,
   FMX.Forms,
+{$IFDEF MSWINDOWS}
+  Windows,
+{$ENDIF}
   Unit1 in 'Unit1.pas' {Form1};
 
+{$SetPEFlags IMAGE_FILE_LARGE_ADDRESS_AWARE}
 {$R *.res}
 
 begin
